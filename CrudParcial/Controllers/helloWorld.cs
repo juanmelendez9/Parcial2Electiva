58e1e6138be3cbaa1dd06021eb336848a237ab0b
juan120500@outlook.com <juan120500@outlook.com>
2022-04-19 23:52:14 -0500

    Primera subida-base de datos

---
+++ CrudParcial/Controllers/helloWorld.cs
@@ -0,0 +1,29 @@
+﻿
+
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace MvcMovie.Controllers
+{
+    public class helloWorld : Controller
+    {
+        // 
+        // GET: /HelloWorld/
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["message"] = "Hola " + name;
+            ViewData["numTimes"] = numTimes;
+            return View(); 
+           
+        }
+    }
+}
---
+++ CrudParcial/Data/CrudParcialContext.cs
@@ -0,0 +1,20 @@
+﻿#nullable disable
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using CrudParcial.Models;
+
+namespace CrudParcial.Data
+{
+    public class CrudParcialContext : DbContext
+    {
+        public CrudParcialContext (DbContextOptions<CrudParcialContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<CrudParcial.Models.Movie> Movie { get; set; }
+    }
+}
